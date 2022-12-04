6d11df1188282ec36ba7ec599acaff4264795dec
Eric Yu <43701041+zbrachinara@users.noreply.github.com>
2022-12-04 17:46:00 -0500

    template file for advent of code

---
+++ aoc_template.rs
@@ -0,0 +1,7 @@
+#!/usr/bin/env rust-script
+
+fn main() {
+
+    let input = std::fs::read_to_string("aoc").expect(r#"Could not find Advent of Code input (should be a sibling file called "aoc")"#);
+
+}
